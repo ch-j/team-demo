6cf73690c27fde0a1fd50c2e9ed7772b20f5cfc3
ch-j <43591118+ch-j@users.noreply.github.com>
2025-06-19 13:17:16 +0000

    update package

--- frontend/src/components/FilterControls.jsx
+++ frontend/src/components/FilterControls.jsx
@@ -80,20 +80,3 @@ function FilterControls({
 }
 
 export default FilterControls;
-        id="filterAlgorithm"
-        placeholder="Filter by Algorithm Name..."
-        value={filters.algorithm}
-        onChange={(e) => onFilterChange('algorithm', e.target.value)}
-      />
-      <input
-        type="text"
-        id="filterDataset"
-        placeholder="Filter by Dataset Name..."
-        value={filters.dataset}
-        onChange={(e) => onFilterChange('dataset', e.target.value)}
-      />
-    </div>
-  );
-}
-
-export default FilterControls;
